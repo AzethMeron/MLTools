f89b0757ea47946dea1f3c7c7a50bfb9f8cc091f
AzethMeron <meronentertainment@gmail.com>
2025-10-19 23:25:48 +0200

    Finally, good structure

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = read_requirements(Path(os.path.join(HERE, "requirements.txt")
 
 setup(
     name="MLTools",
-    version="1.0.0",
+    version="1.2.0",
     packages=find_packages(),
     install_requires=install_requires,
 )
